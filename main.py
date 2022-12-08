685be5ad6d2cc44fe4aa97963857ec4dc2131c28
thuliofmcarvalho@gmail.com <Lenovosl400intel>
2022-12-07 21:43:50 -0300

    Implementado configurações iniciais para colocar a aplicação em um servidor WEB

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,4 +11,6 @@ db = SQLAlchemy(app)
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
--- routes.py
+++ routes.py
@@ -81,7 +81,7 @@ def remove(game_id):
         return "", 404
 
 
-@app.route('/autenticar', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def auth():
     data = request.get_json()
     user_form = data['nome']
